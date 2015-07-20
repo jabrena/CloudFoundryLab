6caea9bae6fbba2a6391c02793e842e86084aaae
unknown <a611654@MC0X4XKC.ww930.my-it-solutions.net>
2015-07-20 13:45:08 +0200

    Using services in the application. https://github.com/jabrena/CloudFoundryLab/issues/9

--- NodeJSHelloWorld/app2.js
+++ NodeJSHelloWorld/app2.js
@@ -11,8 +11,9 @@ var server = http.createServer(function (request, response) {
   //response.end("Hello World\n");
 
     var connection = mysql.createConnection({
-      host     : 'localhost',
+      host     : '127.0.0.1',
       user     : 'root',
+      database : 'test',
       password : '123456',
       database : 'test'
     });
