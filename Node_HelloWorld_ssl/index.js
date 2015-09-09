a0c4c77b8b823ffaa2616ae2383de4be57ea903e
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-09-09 12:35:11 +0200

    Running in CF, but I cant see "Hello Secure World"

--- Node_HelloWorld_ssl/index.js
+++ Node_HelloWorld_ssl/index.js
@@ -1,19 +1,34 @@
 /*jslint node: true*/
 "use strict";
 
-var fs = require('fs'),
-    https = require('https'),
-    express = require('express'),
-    app = express();
-
-var port = process.env.VCAP_APP_PORT || 8000;
-https.createServer({
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var secureServer = require('https').createServer({
     key: fs.readFileSync('privkey.pem'),
     cert: fs.readFileSync('cert.pem')
-}, app).listen(port);
-console.log('Listening on port ' + port + '...');
+}, app);
+var insecureServer = require('http').Server(app);
 
 app.get('/', function (req, res) {
+    console.log(JSON.stringify(req.headers));
+
+/*
+    if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+        res.redirect("https://" + req.headers.host + req.url);
+    }
+*/
     res.header('Content-type', 'text/html');
     return res.end('<h1>Hello, Secure World!</h1>');
 });
+
+var portSecure = process.env.VCAP_APP_PORT || 4443;
+var portInSecure = process.env.VCAP_APP_PORT || 8000;
+secureServer.listen(portSecure, function () {
+    console.log('Listening on *:' + portSecure);
+});
+/*
+insecureServer.listen(portInSecure, function () {
+    console.log('Listening on *:' + portInSecure);
+});
+*/
