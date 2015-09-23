21ce11507f0637854905938be8a56a2c758cf3b5
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-09-23 10:55:21 +0200

    Adding examples to tests http2

---
+++ Node_HelloWorld_http2_express/index.js
@@ -0,0 +1,21 @@
+/*jslint node: true*/
+"use strict";
+
+var fs = require('fs');
+var http2 = require('http2');
+var express = require('express');
+
+var app = express();
+app.get('/', function (req, res) {
+    console.log(req.headers);
+
+    res.header('Content-type', 'text/html');
+    return res.end('<h1>Hello, Secure World!</h1>');
+});
+
+var options = {
+    key: fs.readFileSync('./certificate/privkey.pem'),
+    cert: fs.readFileSync('./certificate/cert.pem')
+};
+var port = process.env.VCAP_APP_PORT || 8080;
+http2.createServer(options, app).listen(port);
--- Node_HelloWorld_ssl/index.js
+++ Node_HelloWorld_ssl/index.js
@@ -8,27 +8,15 @@ var secureServer = require('https').createServer({
     key: fs.readFileSync('privkey.pem'),
     cert: fs.readFileSync('cert.pem')
 }, app);
-var insecureServer = require('http').Server(app);
 
 app.get('/', function (req, res) {
     console.log(JSON.stringify(req.headers));
 
-/*
-    if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
-        res.redirect("https://" + req.headers.host + req.url);
-    }
-*/
     res.header('Content-type', 'text/html');
     return res.end('<h1>Hello, Secure World!</h1>');
 });
 
 var portSecure = process.env.VCAP_APP_PORT || 4443;
-var portInSecure = process.env.VCAP_APP_PORT || 8000;
 secureServer.listen(portSecure, function () {
     console.log('Listening on *:' + portSecure);
 });
-/*
-insecureServer.listen(portInSecure, function () {
-    console.log('Listening on *:' + portInSecure);
-});
-*/
